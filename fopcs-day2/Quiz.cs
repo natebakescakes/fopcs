63fb53394435b228835fb7f8ae08cef3586e6842
natebakescakes <natebakescakes@gmail.com>
2018-02-06 09:47:03 +0800

    Add Day 2 quiz questions and answers

---
+++ fopcs-day2/Quiz.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fopcs_day2
+{
+    public class Quiz
+    {
+        // Not the taxi question, the discount is a mass-discount on the total
+        public static double InitialAnswer(int noOfItems)
+        {
+            double totalCost = 500 * noOfItems;
+            double totalCostAfterDiscount = totalCost;
+
+            if (totalCost > 2000) totalCostAfterDiscount -= Math.Min(1000, totalCost - 2000) * 0.03;
+            if (totalCost > 3000) totalCostAfterDiscount -= Math.Min(3000, totalCost - 3000) * 0.05;
+            if (totalCost > 6000) totalCostAfterDiscount -= (totalCost - 6000) * 0.08;
+
+            return totalCostAfterDiscount;
+        }
+
+        public static double CorrectAnswer(int noOfItems)
+        {
+            double initialCost = 500 * noOfItems;
+
+            if (initialCost > 2000 && initialCost <= 3000)
+            {
+                return initialCost * 0.97;
+            }
+            else if (initialCost > 3000 && initialCost <= 6000)
+            {
+                return initialCost * 0.95;
+            }
+            else if (initialCost > 6000)
+            {
+                return initialCost * 0.92;
+            }
+            else
+            {
+                return initialCost;
+            }
+        }
+
+        public static double AlternativeAnswer(int noOfItems)
+        {
+            double initialCost = 500 * noOfItems;
+
+            if (initialCost > 6000)
+            {
+                return initialCost * 0.92;
+            }
+            else if (initialCost > 3000)
+            {
+                return initialCost * 0.95;
+            }
+            else if (initialCost > 2000)
+            {
+                return initialCost * 0.97;
+            }
+            else
+            {
+                return initialCost;
+            }
+        }
+    }
+}
---
+++ fopcs.Tests/TestQuiz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using fopcs_day2;
+
+namespace fopcs.Tests
+{
+    [TestClass]
+    public class TestQuiz
+    {
+        [TestMethod]
+        public void TestInitialAnswer()
+        {
+            Assert.AreEqual(500, Quiz.InitialAnswer(1));
+            Assert.AreEqual(2425, Quiz.InitialAnswer(5));
+            Assert.AreEqual(4750, Quiz.InitialAnswer(10));
+        }
+
+        [TestMethod]
+        public void TestCorrectAnswer()
+        {
+            Assert.AreEqual(500, Quiz.CorrectAnswer(1));
+            Assert.AreEqual(2425, Quiz.CorrectAnswer(5));
+            Assert.AreEqual(4750, Quiz.CorrectAnswer(10));
+        }
+
+        [TestMethod]
+        public void TestAlternativeAnswer()
+        {
+            Assert.AreEqual(500, Quiz.AlternativeAnswer(1));
+            Assert.AreEqual(2425, Quiz.AlternativeAnswer(5));
+            Assert.AreEqual(4750, Quiz.AlternativeAnswer(10));
+        }
+    }
+}
