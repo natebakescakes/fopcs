52f0bb3f982cafcc8266736095c3655ec08994a5
natebakescakes <natebakescakes@gmail.com>
2018-02-06 09:48:26 +0800

    Rename Day 2 Quiz test

--- fopcs.Tests/TestDay2Quiz.cs
+++ fopcs.Tests/TestDay2Quiz.cs
@@ -5,7 +5,7 @@ using fopcs_day2;
 namespace fopcs.Tests
 {
     [TestClass]
-    public class TestQuiz
+    public class TestDay2Quiz
     {
         [TestMethod]
         public void TestInitialAnswer()
